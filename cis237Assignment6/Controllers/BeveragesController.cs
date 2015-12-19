83932d6876e72e024caadbcd9759a22bcce41800
zacharydwyer <zdwyer4035@mail.kvcc.edu>
2015-12-19 09:20:58 -0500

    Fixed a few bugs

--- cis237Assignment6/Controllers/BeveragesController.cs
+++ cis237Assignment6/Controllers/BeveragesController.cs
@@ -27,14 +27,22 @@ namespace cis237Assignment6.Controllers
             string filterPack = String.Empty;
             string filterMinPrice = String.Empty;
             string filterMaxPrice = String.Empty;
-            bool filterActive = false;
+            string filterActiveStatus = String.Empty;        
+            string filterAmountToShow = String.Empty;       
 
+            // Stores minimum and maximum values for the price.
             int min = 0;
             int max = 3000;
+            int amountToShow = 50;
+            bool isActive = false;
+
 
             // Get data from the session.
+
+            // If the "name" form object in the session exists, and the text value it has is not empty...
             if (Session["name"] != null && !String.IsNullOrWhiteSpace((string)Session["name"]))
             {
+                // Set the filter.
                 filterName = (string)Session["name"];
             }
 
@@ -55,26 +63,90 @@ namespace cis237Assignment6.Controllers
                 max = Int32.Parse(filterMaxPrice);
             }
             
-            // TODO: FIX THIS ERROR
-            // This never seems to be tripped despite being there, which means the filter isn't working properly.
+            // If the "active" drop down box exists in the session...
             if (Session["active"] != null)
             {
-                filterActive = (bool)Session["active"];
+                // Set the filter's text to the "value" amount of what was in the dropdown box. 
+                filterActiveStatus = (string)Session["active"];
+
+                // "0" indicates both. Meaning that isActive won't even be used down the road.
+                if(filterActiveStatus == "0")
+                {
+
+                } else if (filterActiveStatus == "1")
+                {
+                    // 1 = "Get beverages that are active"
+                    isActive = true;
+                } else if (filterActiveStatus == "2")
+                {
+                    // 2 = "Get beverages that are not active"
+                    isActive = false;
+                } else
+                {
+                    // This should never happen, technically. Wasn't sure what type of exception to throw...
+                    throw new FormatException("Value was not 0, 1 or 2");
+                }
             }
 
-            IEnumerable<Beverage> filtered = beveragesToSearch.Where(beverage => beverage.price >= min &&
+            // If there's something in the session (not first time start up)
+            if (Session["atATime"] != null)
+            {
+                // If the form object's text is not empty
+                if (!String.IsNullOrWhiteSpace((string)Session["atATime"])) {
+
+                    // Get the value from the form object
+                    filterAmountToShow = (string)Session["atATime"];
+
+                    // Convert it to a value we can use in the query
+                    amountToShow = Int32.Parse(filterAmountToShow);
+                }
+                // If the form object's text IS empty (but this isn't the first time we've started up)
+                else {
+                    // Set the amount to show to "unlimited" (at least as far as integers are concerned) - they want to show everything if the text field is blank
+                    amountToShow = int.MaxValue;
+                }
+            }
+            // If this is the first time we've started up
+            else if (Session["atATime"] == null)
+            {
+                // Default amount for first time landing
+                filterAmountToShow = "50";
+            }
+
+            // Collection
+            IEnumerable<Beverage> filteredListOfBeverages;
+
+            // Execute query based on whether or not filterActive was used.
+            if (filterActiveStatus == "0")
+            {
+                // Don't include "isActive" filter in query.
+                filteredListOfBeverages = beveragesToSearch.Where(beverage => beverage.price >= min &&
+                                                                                beverage.price <= max &&
+                                                                                beverage.name.Contains(filterName) &&
+                                                                                beverage.pack.Contains(filterPack));
+            } else {
+                // Do include "isActive" in query.
+                filteredListOfBeverages = beveragesToSearch.Where(beverage => beverage.price >= min &&
                                                                                  beverage.price <= max &&
                                                                                  beverage.name.Contains(filterName) &&
                                                                                  beverage.pack.Contains(filterPack) &&
-                                                                                 beverage.active == filterActive);
+                                                                                 beverage.active == isActive);
+            }
+            
+            // Get the list of beverages from the query results.
+            IEnumerable<Beverage> finalFiltered = filteredListOfBeverages.ToList();
 
-            IEnumerable<Beverage> finalFiltered = filtered.ToList();
+            // Restrict the number of items to show at a time. This prevents lag in the browser. 
+            // TODO: If had the time could probably implement some form of a "next page/prev page" for the list but the structure of this application isn't really supportive for it
+            finalFiltered = finalFiltered.Take<Beverage>(amountToShow);
 
+            // Put the values we just potentially retrieved from the Session's form objects into the viewbag; the Form objects will use these as values for themselves.
             ViewBag.filterName = filterName;
             ViewBag.filterPack = filterPack;
             ViewBag.filterMinPrice = filterMinPrice;
             ViewBag.filterMaxPrice = filterMaxPrice;
-            ViewBag.filterActive = filterActive;
+            ViewBag.filterActive = filterActiveStatus;
+            ViewBag.filterAmountToShow = filterAmountToShow;
 
             return View(finalFiltered);
         }
@@ -90,15 +162,8 @@ namespace cis237Assignment6.Controllers
             String pack = Request.Form.Get("pack");
             String min = Request.Form.Get("min");
             String max = Request.Form.Get("max");
-            Boolean isActive = true;
-
-            if(Request.Form["isActive"] == "on")
-            {
-                isActive = true;
-            } else
-            {
-                isActive = false;
-            }
+            String active = Request.Form.Get("active");
+            String atATime = Request.Form.Get("atATime");
 
             //Store the form data into the session so that it can be retrived later
             //on to filter the data.
@@ -106,6 +171,8 @@ namespace cis237Assignment6.Controllers
             Session["pack"] = pack;
             Session["min"] = min;
             Session["max"] = max;
+            Session["active"] = active;
+            Session["atATime"] = atATime;
 
             //Redirect the user to the index page. We will do the work of actually
             //fiiltering the list in the index method.
